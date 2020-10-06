9a8d69bcc1f467e2fec8ae9ab140dd7a4527e011
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-10-06 18:07:25 +0000

    Mumuki Export on 2020-10-06 18:07:25 +0000

--- 00003_Composición/extra.hs
+++ 00003_Composición/extra.hs
@@ -1,5 +1,1 @@
 {-...previousSolution...-}
-
-my_avg :: [Int] -> Int
-my_avg [] = 0
-my_avg xs = sum xs `div` length xs
--- 00003_Composición/test.hs
+++ 00003_Composición/test.hs
@@ -1,27 +1,24 @@
-{-...extra...-}
-
-
 describe "promedio" $ do
   let v_in = []::[Int]
-  let v_out = my_avg v_in
+  let v_out = 0
   let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
     promedio v_in `shouldBe` v_out
 
   let v_in = [1]
-  let v_out = my_avg v_in
+  let v_out = 1
   let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
     promedio v_in `shouldBe` v_out
 
   let v_in = [1,9]
-  let v_out = my_avg v_in
+  let v_out = 5
   let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
     promedio v_in `shouldBe` v_out
 
   let v_in = [0,3,2,-12]
-  let v_out = my_avg v_in
+  let v_out = -2
   let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
     promedio v_in `shouldBe` v_out
