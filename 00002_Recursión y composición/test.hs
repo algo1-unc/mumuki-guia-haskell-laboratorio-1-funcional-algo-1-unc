1973e76dfa041a0cd922d689a8b0402bfc4435d4
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-10-06 09:11:50 +0000

    Mumuki Export on 2020-10-06 09:11:50 +0000

--- 00002_Recursión y composición/test.hs
+++ 00002_Recursión y composición/test.hs
@@ -1,3 +1,5 @@
+{-...extra...-}
+
 describe "paratodo" $ do
   let v_in = []::[Bool]
   let v_out = True
@@ -66,3 +68,47 @@ describe "productoria" $ do
   let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
     productoria v_in `shouldBe` v_out
+
+describe "factorial" $ do
+  let v_in = 0
+  let v_out = my_fac v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    factorial v_in `shouldBe` v_out
+
+  let v_in = 1
+  let v_out = my_fac v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    factorial v_in `shouldBe` v_out
+
+  let v_in = 4
+  let v_out = my_fac v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    factorial v_in `shouldBe` v_out
+
+describe "promedio" $ do
+  let v_in = []::[Int]
+  let v_out = my_avg v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    promedio v_in `shouldBe` v_out
+
+  let v_in = [1]
+  let v_out = my_avg v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    promedio v_in `shouldBe` v_out
+
+  let v_in = [1,9]
+  let v_out = my_avg v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    promedio v_in `shouldBe` v_out
+
+  let v_in = [0,3,2,-12]
+  let v_out = my_avg v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    promedio v_in `shouldBe` v_out
