e7fa9f80e04f49de700c74fa8f88384635a226fb
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-10-06 09:05:28 +0000

    Mumuki Export on 2020-10-06 09:05:28 +0000

--- 00002_Recursión y composición/test.hs
+++ 00002_Recursión y composición/test.hs
@@ -16,3 +16,53 @@ describe "paratodo" $ do
   let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
     paratodo v_in `shouldBe` v_out
+
+describe "sumatoria" $ do
+  let v_in = []::[Int]
+  let v_out = sum v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    sumatoria v_in `shouldBe` v_out
+
+  let v_in = [22]
+  let v_out = sum v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    sumatoria v_in `shouldBe` v_out
+
+  let v_in = [-1,33,-44]
+  let v_out = sum v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    sumatoria v_in `shouldBe` v_out
+
+  let v_in = [2,0,3,4,5,12]
+  let v_out = sum v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    sumatoria v_in `shouldBe` v_out
+
+describe "productoria" $ do
+  let v_in = []::[Int]
+  let v_out = product v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    productoria v_in `shouldBe` v_out
+
+  let v_in = [-1,-33]
+  let v_out = product v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    productoria v_in `shouldBe` v_out
+
+  let v_in = [-3,4]
+  let v_out = product v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    productoria v_in `shouldBe` v_out
+
+  let v_in = [12,0,-7]
+  let v_out = product v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    productoria v_in `shouldBe` v_out
