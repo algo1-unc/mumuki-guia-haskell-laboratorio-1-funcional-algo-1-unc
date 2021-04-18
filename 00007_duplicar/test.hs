494f18884c40374eb66b624027288abf908c9dcf
benjamin.ocampo@mi.unc.edu.ar <benjamin.ocampo@mi.unc.edu.ar>
2021-04-18 19:42:16 +0000

    Mumuki Export on 2021-04-18 19:42:16 +0000

--- 00007_duplicar/test.hs
+++ 00007_duplicar/test.hs
@@ -11,6 +11,11 @@ describe "duplicar" $ do
   it msg $ do
     duplicar v_in `shouldBe` v_out
   
+  let v_in = [-0.4, 0.2, 0, 0.2, 0.4]::[Int]
+  let v_out = map (*2) v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    duplicar v_in `shouldBe` v_out
 
 describe "duplicar'" $ do 
   let v_in = []::[Int]
@@ -24,4 +29,10 @@ describe "duplicar'" $ do
   let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
     duplicar' v_in `shouldBe` v_out
-  
+
+  let v_in = [-0.4, -0.2, 0, 0.2, 0.4]::[Int]
+  let v_out = map (*2) v_in
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    duplicar' v_in `shouldBe` v_out
+
