3310aad3b290b65c7582a016bdf1e2b0924fb6b6
benjamin.ocampo@mi.unc.edu.ar <benjamin.ocampo@mi.unc.edu.ar>
2021-04-18 18:35:04 +0000

    Mumuki Export on 2021-04-18 18:35:04 +0000

---
+++ 00006_Composición/default.hs
@@ -0,0 +1,14 @@
+todosPares :: [Int] -> Bool
+todosPares = undefined
+
+hayMultiplo :: Int -> [Int] -> Bool
+hayMultiplo = undefined
+
+sumaCuadrados :: Int -> Int
+sumaCuadrados = undefined
+
+factorial' :: Int -> Int
+factorial' = undefined
+
+multiplicaPares :: [Int] -> Int
+multiplicaPares = undefined
--- 00006_Composición/test.hs
+++ 00006_Composición/test.hs
@@ -1,4 +1,62 @@
-describe "Group description" $ do 
+describe "todosPares" $ do
+  let v_in = []::[Int]
+  let v_out = True
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    todosPares v_in `shouldBe` v_out
+
+  let v_in = [2, 4, 6, 8, 10]::[Int]
+  let v_out = True
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    todosPares v_in `shouldBe` v_out
+
+  let v_in = [2, 4, 5, 6, 8, 10]::[Int]
+  let v_out = False
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    todosPares v_in `shouldBe` v_out
+
+describe "hayMultiplo" $ do
+  let v_in1 = -10
+  let v_in2 = []::[Int]
+  let v_out = False
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in1) ++ " " ++ show(v_in2)
+  it msg $ do
+    hayMultiplo v_in1 v_in2 `shouldBe` v_out
   
-  it "Sample description" $ do
-    True `shouldBe` True
+  let v_in1 = 12
+  let v_in2 = [13..24]::[Int]
+  let v_out = True
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in1) ++ " " ++ show(v_in2)
+  it msg $ do
+    hayMultiplo v_in1 v_in2 `shouldBe` v_out
+
+  let v_in1 = 25
+  let v_in2 = [13..24]::[Int]
+  let v_out = False
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in1) ++ " " ++ show(v_in2)
+  it msg $ do
+    hayMultiplo v_in1 v_in2 `shouldBe` v_out
+
+describe "sumaCuadrados" $ do
+  let v_in = 10
+  let v_out = sum $ map (\x->x*x) [1..v_in]
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    sumaCuadrados v_in `shouldBe` v_out
+  
+  let v_in1 = 12
+  let v_in2 = [13..24]::[Int]
+  let v_out = True
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in1) ++ " " ++ show(v_in2)
+  it msg $ do
+    hayMultiplo v_in1 v_in2 `shouldBe` v_out
+
+  let v_in1 = 25
+  let v_in2 = [13..24]::[Int]
+  let v_out = False
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in1) ++ " " ++ show(v_in2)
+  it msg $ do
+    hayMultiplo v_in1 v_in2 `shouldBe` v_out
+
