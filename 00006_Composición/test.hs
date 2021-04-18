c818787ea430ed874037d1d2d1941e33ae2f973a
benjamin.ocampo@mi.unc.edu.ar <benjamin.ocampo@mi.unc.edu.ar>
2021-04-18 18:37:31 +0000

    Mumuki Export on 2021-04-18 18:37:31 +0000

--- 00006_Composición/test.hs
+++ 00006_Composición/test.hs
@@ -46,17 +46,22 @@ describe "sumaCuadrados" $ do
   it msg $ do
     sumaCuadrados v_in `shouldBe` v_out
   
-  let v_in1 = 12
-  let v_in2 = [13..24]::[Int]
-  let v_out = True
-  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in1) ++ " " ++ show(v_in2)
+  let v_in = 0
+  let v_out = sum $ map (\x->x*x) [1..v_in]
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
-    hayMultiplo v_in1 v_in2 `shouldBe` v_out
+    sumaCuadrados v_in `shouldBe` v_out
 
-  let v_in1 = 25
-  let v_in2 = [13..24]::[Int]
-  let v_out = False
-  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in1) ++ " " ++ show(v_in2)
+describe "factorial" $ do
+  let v_in = 10
+  let v_out = product $ [1..v_in]
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
-    hayMultiplo v_in1 v_in2 `shouldBe` v_out
+    factorial v_in `shouldBe` v_out
+  
+  let v_in = 0
+  let v_out = product $ [1..v_in]
+  let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
+  it msg $ do
+    factorial v_in `shouldBe` v_out
 
