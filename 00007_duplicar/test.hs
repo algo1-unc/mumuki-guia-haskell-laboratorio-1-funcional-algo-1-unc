effb90de1d96ac010561e319a82cb05dae7d568f
benjamin.ocampo@mi.unc.edu.ar <benjamin.ocampo@mi.unc.edu.ar>
2021-04-18 19:45:46 +0000

    Mumuki Export on 2021-04-18 19:45:46 +0000

--- 00007_duplicar/test.hs
+++ 00007_duplicar/test.hs
@@ -5,13 +5,13 @@ describe "duplicar" $ do
   it msg $ do
     duplicar v_in `shouldBe` v_out
 
-  let v_in = [-10..10]::[Int]
+  let v_in = [-10..10]
   let v_out = map (*2) v_in
   let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
     duplicar v_in `shouldBe` v_out
   
-  let v_in = [-0.4, 0.2, 0, 0.2, 0.4]::[Int]
+  let v_in = [-0.4, 0.2, 0, 0.2, 0.4]
   let v_out = map (*2) v_in
   let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
@@ -24,13 +24,13 @@ describe "duplicar'" $ do
   it msg $ do
     duplicar' v_in `shouldBe` v_out
 
-  let v_in = [-10..10]::[Int]
+  let v_in = [-10..10]
   let v_out = map (*2) v_in
   let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
     duplicar' v_in `shouldBe` v_out
 
-  let v_in = [-0.4, -0.2, 0, 0.2, 0.4]::[Int]
+  let v_in = [-0.4, -0.2, 0, 0.2, 0.4]
   let v_out = map (*2) v_in
   let msg = "Devuelve " ++ show(v_out) ++ " dado " ++ show(v_in)
   it msg $ do
